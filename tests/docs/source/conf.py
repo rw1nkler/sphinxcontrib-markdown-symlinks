ed9c966b9962c26834ccd4c7524e07dc60cc82ec
Robert Winkler <rwinkler@antmicro.com>
2020-06-30 23:19:21 +0200

    Update the test

--- tests/docs/source/conf.py
+++ tests/docs/source/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+from markdown_code_symlinks import LinkParser, MarkdownSymlinksDomain
 
 # -- Project information -----------------------------------------------------
 
@@ -31,6 +32,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,6 +50,11 @@ language = 'python'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+source_parsers = {
+    '.md': 'markdown_code_symlinks.LinkParser',
+}
+
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -60,3 +67,20 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+def setup(app):
+    github_code_repo = 'https://github.com/rw1nkler/sphinxcontrib-markdown-symlinks'
+    github_code_branch = 'fix_images'
+
+    docs_root_dir = os.path.realpath(os.path.dirname(__file__))
+    # Create code root dir relative to docs_root_dir
+    code_root_dir = os.path.realpath(os.path.join(docs_root_dir, "..", ".."))
+
+    MarkdownSymlinksDomain.init_domain(
+        github_code_repo, github_code_branch, docs_root_dir, code_root_dir)
+    MarkdownSymlinksDomain.find_links()
+    app.add_domain(MarkdownSymlinksDomain)
+    app.add_config_value(
+        'recommonmark_config', {
+            'github_code_repo': github_code_repo,
+        }, True)
--- tests/docs/source/index.rst
+++ tests/docs/source/index.rst
@@ -3,18 +3,12 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Markdown Symlinks Tests's documentation!
-===================================================
+Markdown Symlinks Tests
+=======================
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+
+   test_image
 
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
